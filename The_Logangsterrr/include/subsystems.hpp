b8e827cf73400f481905bf1542d204bd17ab3968
L0G4N2 <loganjgutierrez@gmail.com>
2025-09-23 09:37:31 -0500

    Added subsystems.hpp to feel like EZ-Template

---
+++ The_Logangsterrr/include/subsystems.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "api.h"
+#include "lemlib/api.hpp"
+
+// Subsystem instances
+// lemlib::Drive drive;
+// lemlib::Intake intake;
+
+// // Devices
+// inline pros::MotorGroup Intake(14, 15);
+// inline pros::adi::DigitalOut Descore('A');
+// inline pros::Motor HighLow(6);
