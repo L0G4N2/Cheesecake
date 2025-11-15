ac145b617b4b7f6e53c3753f0c9628844cf1e465
L0G4N2 <loganjgutierrez@gmail.com>
2025-11-14 20:55:52 -0600

    Psuedocoding Autonomous

--- Senioritus.exe/src/autons.cpp
+++ Senioritus.exe/src/autons.cpp
@@ -1,34 +1,49 @@
 #include "main.h"
+#include "subsystems.hpp"
 #include "autons.hpp"
 
-void RedRight() {
+void right() {
     // Code here
-    chassis.moveToPoint(50, 20, 4000,{true, 50, 30});
+    chassis.setPose(0, 0, 0);
+    HighLow.move(127);
+    chassis.moveToPose(11.2, 29, 45, 4000);
+    Intake.move(80);
+    pros::delay(500);
+    Intake.move(0);
+    chassis.moveToPose(36, 0, 180, 4000);
+    chassis.moveToPose(36, 17, 180, 4000, {false});
     Intake.move(127);
+    pros::delay(1000);
+    chassis.moveToPose(36, -10, 180, 4000);
+    pros::delay(1000);
+    chassis.moveToPose(36, 17, 180, 4000, {false});
+    Intake.move(0);
+    HighLow.move(0);
 }
 
-void RedLeft() {
+void left() {
     // Code here
-    chassis.moveToPoint(20, 20, 90);
+    chassis.setPose(0, 0, 0);
+    HighLow.move(127);
+    chassis.moveToPose(-11.2, 29, -45, 4000);
+    Intake.move(80);
+    pros::delay(500);
+    Intake.move(0);
+    chassis.moveToPose(-36, 0, 180, 4000);
+    chassis.moveToPose(-36, 17, 180, 4000, {false});
     Intake.move(127);
-}
-
-void BlueRight() {
-    // Code here
-    chassis.moveToPoint(20, 20, 90);
-    Intake.move(127);
-}
-
-void BlueLeft() {
-    // Code here
-    chassis.moveToPoint(20, 20, 90);
-    Intake.move(127);
+    pros::delay(1000);
+    chassis.moveToPose(-36, -10, 180, 4000);
+    pros::delay(1000);
+    chassis.moveToPose(-36, 17, 180, 4000, {false});
+    Intake.move(0);
+    HighLow.move(0);
 }
 
 void odom_test() {
     // Code here
     // set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
+    chassis.setPose(-50, 0, 0);
     // move to point (0, 20) with a very long timeout
-    chassis.moveToPose(0, 20, 20, 100000);
+    chassis.moveToPose(0, 0, 0, 100000);
 }
