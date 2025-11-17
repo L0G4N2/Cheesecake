31f8120f5aa7cf8cd7c2ac6dcca457c5bc26e259
L0G4N2 <loganjgutierrez@gmail.com>
2025-11-17 06:40:19 -0600

    Update subsystems.hpp

--- Senioritus.exe/include/subsystems.hpp
+++ Senioritus.exe/include/subsystems.hpp
@@ -7,4 +7,4 @@ using namespace pros;
 inline Controller master(E_CONTROLLER_MASTER);
 inline MotorGroup Intake({2, -1}); // Creates a motor group for the Intake
 inline Motor HighLow(10);
-inline adi::DigitalOut Blocker('D');
+inline adi::DigitalOut Blocker('A');
