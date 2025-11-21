748769575a8785aba0a5d7853155d2aadf4d43a4
L0G4N2 <loganjgutierrez@gmail.com>
2025-11-20 19:18:00 -0600

    Split the Intake to manipulate both motors

--- Senioritus.exe/include/subsystems.hpp
+++ Senioritus.exe/include/subsystems.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "lemlib/api.hpp"
+#include "pros/abstract_motor.hpp"
 
 using namespace pros;
 
 inline Controller master(E_CONTROLLER_MASTER);
+inline Motor S(2);
+inline Motor Front(-1, pros::MotorGears::blue);
 inline MotorGroup Intake({2, -1}); // Creates a motor group for the Intake
 inline Motor HighLow(10);
 inline adi::DigitalOut Blocker('A');
