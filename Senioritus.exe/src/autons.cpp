61f4f6eddadc2af27d80886ebf847576f3453799
L0G4N2 <loganjgutierrez@gmail.com>
2025-12-03 20:17:26 -0600

    Made an actual working auton for the Right side, just to have to redo it since I have a new bot

--- Senioritus.exe/src/autons.cpp
+++ Senioritus.exe/src/autons.cpp
@@ -8,41 +8,62 @@
 
 void right() {
     // Code here
+    chassis.calibrate(true);
+    pros::delay(1500);
     chassis.setPose(0, 0, 0);
-    HighLow.move(-127);
+    // HighLow.move(-127);
     Blocker.set_value(true);
-    chassis.moveToPose(15, 15, -45, 4000, {.minSpeed = 60, .earlyExitRange = 6});
+    chassis.moveToPose(15, 22.5, -45, 4000, {.minSpeed = 60, .earlyExitRange = 6});
     chassis.waitUntilDone();
     Front.move(127);
     S.move(60);
-    chassis.moveToPoint(-25, 30, 6000, {.maxSpeed = 40});
-    chassis.swingToHeading(-90, lemlib::DriveSide::RIGHT, 3000, {.maxSpeed = 50});
+    chassis.moveToPoint(-25, 30, 4000, {.maxSpeed = 40});
+    chassis.swingToHeading(-80, lemlib::DriveSide::LEFT, 3000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE, .maxSpeed = 50});
+    S.move(30);
+    chassis.waitUntilDone();
+    chassis.swingToHeading(-45, lemlib::DriveSide::LEFT, 3000, {.direction = AngularDirection::CW_CLOCKWISE});
+    chassis.moveToPoint(-25, 30, 4000, {.forwards = false, .maxSpeed = 40});
+    chassis.moveToPoint(-30, 35, 4000);
+    Intake.move(-127);
+    // chassis.moveToPoint(-25, 30, 4000, {.forwards = false, .maxSpeed = 40});
+    // chassis.swingToHeading(-50, lemlib::DriveSide::LEFT, 4000, {.direction = lemlib::AngularDirection::CCW_COUNTERCLOCKWISE});
+    // chassis.moveToPoint(-35, 35, 4000, {.minSpeed = 127});
+}
+
+void rightWP() {
+    // Code here
 }
 
 void left() {
     // Code here
+    chassis.calibrate(true);
     chassis.setPose(0, 0, 0);
-    HighLow.move(-127);
+    // HighLow.move(-127);
     Blocker.set_value(true);
-    chassis.moveToPose(-15, 18, 45, 4000, {.minSpeed = 40, .earlyExitRange = 6});
+    chassis.moveToPose(-15, 22.5, 45, 4000, {.minSpeed = 60, .earlyExitRange = 6});
     chassis.waitUntilDone();
-    chassis.moveToPoint(20, 35, 4000, {.maxSpeed = 20});
+    Front.move(127);
+    S.move(60);
+    chassis.moveToPoint(25, 30, 4000, {.maxSpeed = 40});
+    chassis.swingToHeading(80, lemlib::DriveSide::RIGHT, 3000, {.direction = AngularDirection::CW_CLOCKWISE, .maxSpeed = 50});
+    S.move(30);
+    chassis.waitUntilDone();
+    chassis.swingToHeading(45, lemlib::DriveSide::RIGHT, 3000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE});
+    chassis.moveToPoint(25, 30, 4000, {.forwards = false, .maxSpeed = 40});
+    chassis.moveToPoint(30, 35, 4000);
+    Intake.move(127);
+}
 
-    // Intake.move(50);
-    // pros::delay(500);
-    // Intake.move(0);
-    // chassis.turnToHeading(45-180, 4000, {.maxSpeed = 90});
-    // chassis.moveToPose(-3.2, 32.2, 45-180, 4000, {false});
-    // Intake.move(127);
-    // pros::delay(1500);
-    // chassis.moveToPose(-35.4, -7.1, 180, 4500);
+void leftWP() {
+    // Code here
 }
 
 void odom_test() {
     // Code here
     // set position to x:0, y:0, heading:0
-    // chassis.setPose(0, 0, 0);
-
+    chassis.calibrate(true);
+    pros::delay(1500);
+    chassis.setPose(0, 0, 0);
     chassis.moveToPose(0, 50, 0, 4000);
     pros::delay(2000);
     chassis.moveToPose(50, 50, 90, 4000);
