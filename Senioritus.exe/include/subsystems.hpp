b0cb20e6baff213492bb780b93f54e2eef071c2c
L0G4N2 <loganjgutierrez@gmail.com>
2025-12-06 19:09:45 -0600

    Update subsytems

--- Senioritus.exe/include/subsystems.hpp
+++ Senioritus.exe/include/subsystems.hpp
@@ -6,10 +6,9 @@
 using namespace pros;
 
 inline Controller master(E_CONTROLLER_MASTER);
-inline Motor Front(-1, MotorGears::blue);
-inline Motor S(-10, MotorGears::green);
-inline MotorGroup Intake({-1, -10});
+inline Motor Intake(-1, MotorGears::blue);
+inline Motor Blocker(15, MotorGears::blue);
 // inline Motor HighLow(10);
-inline adi::DigitalOut HighLow('A');
-inline adi::DigitalOut Blocker('B');
-inline adi::DigitalOut Scraper('C');
+inline adi::DigitalOut HighLow('G');
+inline adi::DigitalOut Scraper('H');
+inline adi::DigitalOut Descore('C');
