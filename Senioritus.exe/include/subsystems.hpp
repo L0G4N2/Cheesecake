d30a3ef5b60f40a6f0fd32f011ace467426759a9
L0G4N2 <loganjgutierrez@gmail.com>
2026-01-30 23:59:56 -0600

    Added Descore Mech

--- Senioritus.exe/include/subsystems.hpp
+++ Senioritus.exe/include/subsystems.hpp
@@ -8,7 +8,6 @@ using namespace pros;
 inline Controller master(E_CONTROLLER_MASTER);
 inline Motor Intake(-1, MotorGears::blue);
 inline Motor Blocker(15, MotorGears::blue);
-// inline Motor HighLow(10);
-inline adi::DigitalOut HighLow('G');
+
+inline adi::DigitalOut Descore('G');
 inline adi::DigitalOut Scraper('H');
-inline adi::DigitalOut Descore('C');
